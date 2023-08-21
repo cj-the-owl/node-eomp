71b7a9dd68c3a49dd1439bd9aee0b8a0a3239cce
cj-the-owl <lodewykcarynjayn@gmail.com>
2023-08-21 14:56:58 +0200

    landing page almost done

---
+++ node-eomp-frontend/src/components/navBarComp.vue
@@ -0,0 +1,33 @@
+<template>
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">Logo</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNav">
+      <ul class="navbar-nav">
+        <li class="nav-item">
+          <router-link class="nav-link" to="">About</router-link>
+        </li>
+        <li class="nav-item">
+          <router-link class="nav-link" to="">Products/Menu</router-link>
+        </li>
+        <li class="nav-item">
+          <router-link class="nav-link" to="">Contact</router-link>
+        </li>
+        <li class="nav-item">
+          <router-link class="nav-link" to="">Admin</router-link>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
+    
+</template>
+
+<script>
+    export default {
+
+    }
+</script>
--- node-eomp-frontend/src/views/HomeView.vue
+++ node-eomp-frontend/src/views/HomeView.vue
@@ -1,6 +1,45 @@
 <template>
-  
+  <div class="position-relative overflow-hidden text-center text-black" id="hero">
+    <div class="col-md-5 p-lg-5 mx-auto my-5">
+      <h1 class="display-5">this is the name of the store</h1>
+      <button class="btn btn-outline-secondary fw-medium" id="shop-btn">View Products/Menu</button>
+    </div>
+  </div>
+
+  <div class="p-2 m-2">
+    <h3 class="text-center">Some of of our products!</h3>
+    <div id="feat-prod">
+      <img class="p-2" src="https://i.postimg.cc/ZKP0TNZ0/7-RECETAS-PARA-CENAS-LIGERAS-BARATAS-Y-F-CILES-DE-PREPARAR-EN-UN-MOMENTO.jpg">
+      <img class="p-2" src="https://i.postimg.cc/5tDf591Z/How-To-Make-Onigiri-4-Easy-Recipes-Pickled-Plum.jpg">
+      <img class="p-2" src="https://i.postimg.cc/tT7VJ4kw/download-2.jpg">
+    </div>
+  </div>
 </template>
 
+<script>
+  export default {
+    
+  }
+</script>
 
+<style scoped>
+  #hero {
+    background-image: url(https://i.postimg.cc/MpCNhptz/Free-Vector-Hand-drawn-japanese-food-background-removebg-preview.png);
+    background-repeat: no-repeat;
+    background-size:contain;
+    height: 100vh;
+    overflow: hidden;
+    
+  }
 
+  .p-2 {
+    height: 450px;
+  }
+
+  #feat-prod {
+    display: grid;
+    grid-template-columns: auto auto auto;
+    justify-content: center;
+    gap: 20px;
+  }
+</style>
