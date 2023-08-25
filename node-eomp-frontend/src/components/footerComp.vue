b5f2d769d2c843313225844063d6e1f754922270
cj-the-owl <130046870+cj-the-owl@users.noreply.github.com>
2023-08-25 20:41:47 +0200

    added social links

--- node-eomp-frontend/src/components/footerComp.vue
+++ node-eomp-frontend/src/components/footerComp.vue
@@ -3,9 +3,9 @@
         <div>
             <p class="p-2">Copyright ©️ cj-the-owl & Tim</p>
 
-            <img class="px-2" src="https://i.postimg.cc/9Fh3kdxb/icons8-instagram-50-2.png">
-            <img class="px-2" src="https://i.postimg.cc/bJFC1FLy/icons8-github-50-3.png">
-            <img class="px-2" src="https://i.postimg.cc/256NwCHf/icons8-linkedin-50-2.png">
+            <a href="https://www.instagram.com/themysticwok/"><img class="px-2" src="https://i.postimg.cc/9Fh3kdxb/icons8-instagram-50-2.png"></a>
+            <a href="https://github.com/cj-the-owl"><img class="px-2" src="https://i.postimg.cc/bJFC1FLy/icons8-github-50-3.png"></a>
+            <a href="https://www.linkedin.com/in/caryn-jayn-lodewyk-411165276/"><img class="px-2" src="https://i.postimg.cc/256NwCHf/icons8-linkedin-50-2.png"></a>
         </div>
        
     </footer>
